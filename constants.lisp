afe6fc641696b5b3ef879b71ff20dad406192473
Brian O'Reilly <fade@deepsky.com>
2011-10-20 13:50:43 -0400

    create a special to hold the *area-codes* db in the form of an alist.

---
+++ constants.lisp
@@ -0,0 +1,37 @@
+(in-package :harlie)
+
+(defparameter *constants* (merge-pathnames  "SourceCode/lisp/harlie/constants/"
+					    (make-pathname :directory
+							   (pathname-directory
+							    (user-homedir-pathname)))))
+
+;;; area codes
+
+(defun constant-file (fname)
+  "take the filename of a file in the constants directory and return
+   it. Fail with nil if it doesn't exist."
+  (let ((fname (merge-pathnames fname *constants*)))
+    (if (cl-fad:file-exists-p fname)
+	fname
+	nil)))
+
+(defun area-codes ()
+  "return an alist of the country codes established by the
+   international telecomunications union."
+  (let ((ccode-alist nil))
+    (with-open-file (s (constant-file "country_codes")
+		       :direction :input
+		       :external-format :ISO-8859-1)
+      (loop for cline = (read-line s nil nil)
+	    :while cline
+	    ;; :do (format t "~&|~A" cline)
+	    :do (let ((ccode (strip-spaces (split-sequence #\: cline))))
+		  ;; (format t "~&~A" ccode)
+		  (push (cons (parse-integer
+			       (car ccode) :junk-allowed t) (cadr ccode)) ccode-alist))
+	    :finally (return ccode-alist)))))
+
+(defparameter *area-codes* (area-codes)
+  "an alist of the ITU specified area codes.")
+
+;;; /area codes

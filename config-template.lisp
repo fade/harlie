4241a3cd562f4b315a505e52e1d85b60bb0cebd2
Keith Browne <tuxedo@deepsky.com>
2011-10-31 00:31:02 -0400

    More last-minute changes to config-template.lisp.

--- config-template.lisp
+++ config-template.lisp
@@ -6,11 +6,11 @@
 
 (defstruct config
   irc-server-name
-  irc-nick
-  irc-channel-names
+  irc-nick ;Deprecated
+  irc-channel-names ;Deprecated
   irc-nickchannels
   web-server-name
-  web-server-port
+  web-server-port ; Deprecated
   web-server-ports
   url-store-type
   psql-old-credentials
@@ -18,27 +18,12 @@
   psql-chain-credentials
   psql-context-credentials)
 
-(defparameter *shogun-config*
-  (make-config :irc-server-name "irc.srh.org"
-	       :irc-nick "shogun"
-	       :irc-channel-names '("#walled")
-	       :web-server-name "coruscant.deepsky.com"
-	       :web-server-port 5783
-	       :url-store-type :psql
-	       :psql-old-credentials '(("shogundb" "semaphor" nil :unix)
-				       ("botdb" "semaphor" nil :unix)
-				       ("bootsydb" "semaphor" nil :unix)
-				       ("thugdb" "semaphor" nil :unix))
-	       :psql-url-new-credentials '("shogundb" "semaphor" nil :unix)
-	       :psql-chain-credentials '("shogundb" "semaphor" nil :unix)
-	       :psql-context-credentials '("shogundb" "semaphor" nil :unix)))
-
 (defparameter *harlie-config*
   (make-config :irc-server-name "irc.srh.org"
-	       :irc-nick "Harlie"
+	       :irc-nickchannels '(("Harlie" ("#trinity")))
 	       :irc-channel-names '("#trinity")
 	       :web-server-name "coruscant.deepsky.com"
-	       :web-server-port 5791
+	       :web-server-port '(5791)
 	       :url-store-type :psql
 	       :psql-old-credentials '(("botdb" "semaphor" nil :unix)
 				       ("bootsydb" "semaphor" nil :unix)
@@ -50,22 +35,20 @@
 
 (defparameter *harlot-config*
   (make-config :irc-server-name "irc.srh.org"
-	       :irc-nick "Harlot"
-	       :irc-channel-names '("#triscuit")
-	       :web-server-name "coruscant.deepsky.com"
-	       :web-server-port 5791
+	       :irc-nickchannels '(("Harlot" ("#trinity" "#triscuit")))
+	       :web-server-name "127.0.0.1"
+	       :web-server-ports '(5791)
 	       :url-store-type :psql
 	       :psql-old-credentials '(("botdb" "tuxedo" nil :unix))
-	       :psql-url-new-credentials '("harliedb" "tuxedo" nil :unix)
+	       :psql-url-new-credentials '("botdb" "tuxedo" nil :unix)
 	       :psql-chain-credentials '("botdb" "tuxedo" nil :unix)
 	       :psql-context-credentials '("botdb" "tuxedo" nil :unix)))
 
 (defparameter *sr-4-config*
   (make-config :irc-server-name "irc.srh.org"
-	       :irc-nick "SR-4"
-	       :irc-channel-names '("#trinity")
+	       :irc-nickchannels '(("SR-4" ("#trinity")))
 	       :web-server-name "localhost"
-	       :web-server-port 5791
+	       :web-server-ports '(5791)
 	       :url-store-type :psql
 	       :psql-old-credentials '(("botdb" "fade" nil :unix))
 	       :psql-url-new-credentials '("botdb" "fade" nil :unix)
@@ -86,6 +69,4 @@
                :psql-chain-credentials '("botdb" "semaphor" nil :unix)
                :psql-context-credentials '("botdb" "semaphor" nil :unix)))
 
-;(defparameter *bot-config* *harlie-config*)
-(defparameter *bot-config* *combined-config*)  ; Gotta define this in the REPL before running the bot.
-
+(defparameter *bot-config* *combined-config*)

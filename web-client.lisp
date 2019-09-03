2de5a2790874a86a09df479e02caada3ed7a43be
Brian O'Reilly <fade@deepsky.com>
2019-09-03 10:15:05 -0400

    update user agent strings for currency as well as variety.

--- web-client.lisp
+++ web-client.lisp
@@ -75,7 +75,11 @@ Only the first match is returned."
 					 (string-equal (second (car (second x))) "uccResultAmount")))
 		      #'(lambda (y) (third y)))))
 
-(defparameter *user-agents* '("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0" "Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.0.1" "Bite Me"))
+(defparameter *user-ragents* '("Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"
+                               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36"
+                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.2 Safari/605.1.15"
+                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"))
 
 (defun fetch-title (url)
 "Extract the title from a Web page.  Return three values.

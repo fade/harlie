a9bb07eb42464b3e4b249df8b20f430c87546734
Brian O'Reilly <fade@deepsky.com>
2024-04-12 18:13:19 -0400

    the order of these things matters only in aesthetics, still here we are.

--- config-template.lisp
+++ config-template.lisp
@@ -7,16 +7,15 @@
 ;; this sets the logging verbosity. 
 (log:config :debug)
 
+(defparameter *threads* 20 "The threads to use to check stored URLs in parallel.")
+
 ;; the phrase the bot utters to self-identify to other bots (and people). This must be
 ;; coordinated with other bot herders.
-(defparameter *threads* 20 "The threads to use to check stored URLs in parallel.")
-(defparameter *ignore-phrase* "NOTIFY:: Help, I'm a bot!")
+(defparameter *ignore-phrase* "NOTIFY:: Help, I'm a bot!" "Bot emits this phrase on channel join to announce its botness.")
 
 ;; database access credentials.
 (defparameter *bot-database-credentials*
   '("botdb" "your-username" nil :unix))
-
-(defparameter *ignore-phrase* "NOTIFY:: Help, I'm a bot!")
 
 ;; these are exmaples.
 (defparameter *harlot-config*

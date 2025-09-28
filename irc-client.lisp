5335022efe1c0e550e634155275c40b45f01dcd3
Brian O'Reilly <fade@deepsky.com>
2025-09-28 08:42:41 -0400

    if you don't name it you don't know it.

--- irc-client.lisp
+++ irc-client.lisp
@@ -494,10 +494,10 @@ a specific user in a specific channel."))
     ;; the nick is changing, so we need to update the channel-user
     ;; in the bot-irc-channel object appropriately.
 
-    (with-handle-swap sender new-nick
+    (with-handle-swap old-nick new-nick
       ;; (log:debug "~&[USER OBJECT FOR NICK CHANGE] -> ~A" (describe this-user))
-      (log:debug "~2&[NICK CHANGE ~A -> ~A] -- ~{~A~^ ~}~2%"
-                 sender new-nick
+      (log:debug "~2&[NICK CHANGE ~A:~A -> ~A] -- ~{~A~^ ~}~2%"
+                 sender old-nick new-nick
                  (list connection sender new-nick channel text token-text-list command))
       (setf (current-handle this-user) new-nick
             (prev-handle this-user) old-nick
